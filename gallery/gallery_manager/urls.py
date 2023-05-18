3c12c550bccf1baf8fca4ce95b6dd954b05140d1
Kumararaja Sethuraja <kumararaja.sethuraja@ideas2it.com>
2023-05-18 12:24:51 +0530

    Image file validations and storing image in database

--- gallery/gallery_manager/urls.py
+++ gallery/gallery_manager/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('ViewPhoto/<str:pk>/',views.view_photo,name='view_photo'),    
-    path('imageupload',views.image_upload,name='image_upload'),
-    path('gallery',views.gallery,name='gallery'),
+    path('ImageUpload',views.image_upload,name='image_upload'),
+    path('Gallery',views.gallery,name='gallery'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
